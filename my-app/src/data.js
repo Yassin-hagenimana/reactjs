2d49b2372594c6497943a5288387029e42f954e4
yassin <hyassin509@gmail.com>
2022-03-01 22:22:48 +0200

    markdown

---
+++ my-app/src/data.js
@@ -0,0 +1,21 @@
+export const data=[
+{
+    id:1,
+    body: `## this Just in
+    MarkDown is an *awesome* way to write **very basic Html**  by typing manually
+    
+    It can be used for:
+    1.README files
+    2.Slack and Discord messages
+    
+    ----
+    
+    Here's division
+    `
+},
+{id:2,body:`a`},
+{id:3,body:`b`},
+{id:4,body:`c`},
+{id:5,body:`d`},
+{id:6,body:`e`}
+]
